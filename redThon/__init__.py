bda49888d447fbd9e80cfca79d8f253860ee5b95
sunwonsara <sunwonsara@naver.com>
2022-11-06 00:09:56 +0900

    로그인, 회원가입, 아이디찾기, 비밀번호 찾기 구현

--- redThon/__init__.py
+++ redThon/__init__.py
@@ -3,12 +3,16 @@ from flaskext.mysql import MySQL
 from dotenv import load_dotenv
 import os
 from redThon.pybo.forms import UserCreateForm
+import win32api
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def root():
-    return render_template('index.html', title = "main")
+    if request.method == 'GET':
+        return render_template("index.html")
+    else:
+        return render_template('test_image.html', title = "main")
 
 @app.route("/mainmap")
 def mainmap():
@@ -21,10 +25,28 @@ def header():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        print("여기111")
         return render_template("register.html")
     else:
+        userid = request.form.get('userid') 
         return redirect('/')
+
+@app.route("/idfind", methods=['GET', 'POST'])
+def idfind():
+    if request.method == 'GET':
+        return render_template('id_find.html', title = "idfind")
+    else:
+        return "아이디는 0000 입니다." #방법1)하나의 페이지 생성됨 > '되돌아기' 버튼 필요 -> 메인으로 이동 해야함...
+                                      #방법2)하나의 페이지가 새로 띄워지는게 아니라, id_find.html페이지에서 밑에 "문구" 나타나는 형식으로 처리
+
+    
+@app.route("/pwfind", methods=['GET', 'POST'])
+def pwfind():
+    if request.method == 'GET':
+        return render_template('pw_find.html', title = "pwfind")
+    else:
+        return "비밀번호는 0000 입니다." #방법1)하나의 페이지 생성됨 > '되돌아기' 버튼 필요 -> 메인으로 이동 해야함...
+                                        #방법2)하나의 페이지가 새로 띄워지는게 아니라, id_find.html페이지에서 밑에 "문구" 나타나는 형식으로 처리
+
 
 
 @app.route("/signup", methods=['GET', 'POST'])
