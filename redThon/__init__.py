76ee42e39ff9affdd3fb4588bf832ef99fd6a2e1
sunwonsara <sunwonsara@naver.com>
2022-11-05 10:48:30 +0900

    로그인 및 메인맵

--- redThon/__init__.py
+++ redThon/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, render_template, request
-
+from flaskext.mysql import MySQL
+from dotenv import load_dotenv
+import os
 from redThon.pybo.forms import UserCreateForm
 
 app = Flask(__name__)
@@ -7,6 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return render_template('index.html', title = "main")
+
+@app.route("/mainmap")
+def mainmap():
+    return render_template('test_image.html', title = "mainmap")
 
 @app.route("/header")
 def header():
@@ -32,3 +38,20 @@ def signup():
 #    import auth_views
 #    app.register_blueprint(auth_views.bp)
 #    return app
+
+
+#류재범 추가: DB 연동
+load_dotenv(verbose=True)
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = os.getenv('REDTHON_DATABASE_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('REDTHON_DATABASE_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('REDTHON_DATABASE_DB')
+app.config['MYSQL_DATABASE_HOST'] = os.getenv('REDTHON_DATABASE_HOST')
+mysql.init_app(app)
+
+db = mysql.connect()
+cursor = db.cursor();
+cursor.execute("SELECT * FROM test_table WHERE 1")
+connect_test = cursor.fetchone()
+print(connect_test)
